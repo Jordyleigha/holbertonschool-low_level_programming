364ab21c845cbb0f53c7b282d5e0871e33eb74c1
Jordan Arguelles <jordan.arguelles.ja@gmail.com>
2023-11-08 12:54:21 -0800

    fixing my code in the function

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -7,13 +7,13 @@
  * @h: singly linked list
  * Return: the number of nodes
  */
-size_t print_list(const list_t *h);
+size_t print_list(const list_t *h){
 
 size_t elem;
 
 elem = 0;
 while (h != NULL)
-{
+
 	if (h -> str == NULL)
 		printf("[%d] %s\n", 0, "(nil");
 	else
