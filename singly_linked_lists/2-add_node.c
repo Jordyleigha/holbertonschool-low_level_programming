e3f0b74676744727fba748a6257d234eb7d42535
Jordan Arguelles <jordan.arguelles.ja@gmail.com>
2023-11-08 15:59:34 -0800

    writing a function thatnadds a new node at the beggining of a listt list

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -5,4 +5,21 @@
  * @str: string to store in the list
  * Return: the address of the new element.
  */
-list_t *add_node(list_t **head, const char *str);
+list_t *add_node(list_t **head, const char *str);{
+	list_t *add_node;
+	size_t i;
+
+	add_node = malloc(sizeof(list_t));
+	if (add_node == NULL)
+		return (NULL);
+
+	add_node->str = strup(str);
+
+	for (i = 0; str[i]; i++);
+
+	add_node->len = i;
+	add_node->next = *head;
+	*head = add_node;
+
+	return (*head);
+}
