ff076275a2d2a413b11201e13829fbd21516a81a
Jordan Arguelles <jordan.arguelles.ja@gmail.com>
2023-11-08 12:56:21 -0800

    fixing my errors to betty style

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -11,14 +11,15 @@ size_t print_list(const list_t *h){
 
 size_t elem;
 
-elem = 0;
+elem = 0;{
 while (h != NULL)
 
 	if (h -> str == NULL)
 		printf("[%d] %s\n", 0, "(nil");
 	else
 		printf("[%d] %s\n", h->len, h->str);
-	h = h->next
+	h = h->next;
 		elem++;
 	return (elem); 
+}
 }
