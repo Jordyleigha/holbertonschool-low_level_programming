acab65957da306bb58011f8af0abdd10c65df8ae
Jordan Arguelles <jordan.arguelles.ja@gmail.com>
2023-11-08 14:28:45 -0800

    returning the number of elements in a singly linked list

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,8 +1,8 @@
 #include "lists.h"
 /*
- * list_len - returns the number of elements in a linked list_t list
- * @h: singly linked list
- * Return: number of elements
+ * list_len - returns the number of elements in a linked list_t list.
+ * @h: singly linked list.
+ * Return: number of elements.
  */
 
 size_t list_len(const list_t *h)
