1ac0e38099078429e3976d2cbae10c925f4202f7
Jordan Arguelles <jordan.arguelles.ja@gmail.com>
2023-11-08 14:39:11 -0800

    writing my declarations and prototype

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,8 @@
+#include "lists.h"
+/**
+ * add_node - adds a new node at the beginning of a list_t list.
+ * @head: head of the linked list.
+ * @str: string to store in the list
+ * Return: the address of the new element.
+ */
+list_t *add_node(list_t **head, const char *str);
