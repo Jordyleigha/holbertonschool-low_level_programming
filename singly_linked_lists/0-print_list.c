c14106668853eed7607f18b41b3179c7940c77bb
Jordan Arguelles <jordan.arguelles.ja@gmail.com>
2023-11-08 14:05:09 -0800

    missing a space after declarations

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,15 +1,14 @@
 #include "lists.h"
-
 /**
  * print_list - prints all the elements of the list list_t
  * @h: singly linked list
- *
  * Return: the number of nodes
  */
 
 size_t print_list(const list_t *h)
 {
 	size_t elem;
+
 	elem = 0;
 	while (h != NULL)
 	{
