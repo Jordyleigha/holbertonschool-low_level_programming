64ecfb3cd13bffd84bf8f1a5b0f3453ad3678e91
Jordan Arguelles <jordan.arguelles.ja@gmail.com>
2023-11-08 16:03:03 -0800

    replacing my brackets

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -5,7 +5,8 @@
  * @str: string to store in the list
  * Return: the address of the new element.
  */
-list_t *add_node(list_t **head, const char *str); {
+list_t *add_node(list_t **head, const char *str);
+{
 	list_t *add_node;
 	size_t i;
 
