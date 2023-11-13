48919e6f0cc10af33817b76435324012f3340009
Jordan <jordan.arguelles.ja@gmail.com>
2023-11-13 15:01:17 -0800

    not finished yet

---
+++ more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+/**
+ * pop_listint - deletes the head node of a listint_t list
+ * @head: head of the linked list
+ *
+ * Return: data that was in the deleted node, or 0 if the list is empty
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+	int i;
+
+	if (!head || !*head)
+		return (0);
