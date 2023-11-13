c234d505e4bc27660ab9ba704b974980af0485ee
Jordan <jordan.arguelles.ja@gmail.com>
2023-11-13 15:30:48 -0800

    function that returns the sum of all data in a listintt list

---
+++ more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * sum_listint - the sum of all the data in a listint_t list
+ * @head: head of the linked list
+ *
+ * Return: resulting sum
+ */
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+	listint_t *current = head;
+
+	while (current != NULL)
+	{
+		sum += current->n;
+		current = current->next;
+	}
+	return (sum);
+}
