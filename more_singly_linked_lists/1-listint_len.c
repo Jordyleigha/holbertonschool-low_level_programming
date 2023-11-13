bba759bba0d87f1f24fa44898faab6a71daf71ce
Jordan <jordan.arguelles.ja@gmail.com>
2023-11-13 14:10:59 -0800

    writing a function that returns the elements in a linked listint_t list

---
+++ more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * listint_len - returns the number of elements in a linked listint_t list
+ * @h: singly linked list
+ *
+ * Return: number of elements.
+ */
+
+size_t listint_len(const listint_t *h);
+{
+	size_t elem;
+
+	elem = 0;
+	while (h)
+	{
+		elem++;
+		h = h->next;
+	}
+	return (elem);
+}
