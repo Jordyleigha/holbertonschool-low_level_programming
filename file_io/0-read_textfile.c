f1b3af32c823a38bee5f0513c92c12b44f2a44e7
Jordan <jordan.arguelles.ja@gmail.com>
2023-11-13 18:58:20 -0800

    see if they format works for me

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -8,4 +8,28 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters);
 {
+	int ja;
+	ssize_t red, wrt;
+	char *school;
 
+	if (!filename)
+		return (0);
+
+	ja = open(filename, O_RDONLY);
+
+	if (ja == -1)
+		return (0);
+
+	school = malloc(sizeof(char) * (letters));
+	if (!school)
+		return (0);
+
+	red = read(ja, school, letters);
+	wrt = wrtie(STDOUT_FILENO, school, red);
+
+	close (ja);
+
+	free(school);
+
+	return (wrt);
+}
