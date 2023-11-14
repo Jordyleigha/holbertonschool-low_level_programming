0bbc17e034ae4f4fe3ea7714a4f6e31c5da30b69
Jordan <jordan.arguelles.ja@gmail.com>
2023-11-13 16:39:37 -0800

    just writing my prototype

---
+++ bit_manipulation/0-binary_to_unit.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * binary_to_unit - converts a binary number to unsigned int
+ * @b: the string containing the binary number
+ *
+ * Return: the converted number, or 0
+ */
+unsigned int binary_to_unit(const char *b)
+{
