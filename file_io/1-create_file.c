db427a50029c956488cfece4e154f40afe779e4e
Jordan <jordan.arguelles.ja@gmail.com>
2023-11-13 19:11:45 -0800

    making a file

---
+++ file_io/1-create_file.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * create_file - file to be created
+ * @filename: filename
+ * @text_content: NULL terminated string to write to the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int ja = open;
+
+	if (ja == -1)
+	{
+		return (1);
+	}
+}
