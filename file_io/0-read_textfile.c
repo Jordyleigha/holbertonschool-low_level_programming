66657e2b8a56622639f8ce27db8fff8f10f2347e
Jordan <jordan.arguelles.ja@gmail.com>
2023-11-13 18:49:41 -0800

    writing my prototype and descriptions

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * read_textfile - reads a textfile
+ * @filename: filename
+ * @letters: number of letters printed
+ *
+ * Return: the number of letters printed, if failed return 0
+ */
+ssize_t read_textfile(const char *filename, size_t letters);
+{
+
