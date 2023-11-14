88d7773d5ed215baf02e622cbc7a96b1ec50b475
Jordan <jordan.arguelles.ja@gmail.com>
2023-11-13 19:24:03 -0800

    plss work

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -8,10 +8,23 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int ja = open;
+	int jordanfile;
+	unsigned int i;
 
-	if (ja == -1)
+	if (filename == NULL)
+		return (-1);
+
+	jordanfile = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+
+	if (jordanfile == -1)
+		return (-1);
+
+	if (text_content != NULL)
 	{
-		return (1);
+		for (i = 0; i < strlen(text_content); i++)
+		{
+			write(jordanfile, &text_content[i], 1);
+		}
 	}
+	return (1);
 }
