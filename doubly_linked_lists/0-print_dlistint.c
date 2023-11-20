f95b7858ec471b949060d16842855e5876e2f0f3
Jordan <jordan.arguelles.ja@gmail.com>
2023-11-20 09:25:59 -0800

    printing all elements of the list

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -4,4 +4,14 @@
  * @h: singly linked list
  * Return: the number of nodes
  */
-
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t elem;
+	elem = 0;
+	while (h != NULL)
+	{
+		h = h->next;
+		elem++;
+	}
+	return(elem);
+}
