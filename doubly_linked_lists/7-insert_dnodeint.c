87fe8b5388e19a2b7ad7df44e38714c608d90865
Jordan <jordan.arguelles.ja@gmail.com>
2023-11-20 11:42:40 -0800

    fixing spacing

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -22,7 +22,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			return (NULL);
 	}
 
-	if (temp ->next == NULL)
+	if (temp->next == NULL)
 		return (add_dnodeint_end(h, n));
 
 	add = malloc(sizeof(dlistint_t));
