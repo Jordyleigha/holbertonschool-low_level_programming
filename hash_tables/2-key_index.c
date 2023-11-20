60b30e27bff7d77cc66a94f0600675458563c4e7
Jordan <jordan.arguelles.ja@gmail.com>
2023-11-20 14:31:32 -0800

    fixing whitespace

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -1,6 +1,6 @@
 #include "hash_tables.h"
 /**
- * key_index - get the index at which a key/value should be 
+ * key_index - get the index at which a key/value should be
  * stored in array of a hasht table
  * @key: the key to get the index of
  * @size: the size of the array
@@ -10,5 +10,5 @@
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	return(hash_djb2(key) % size);
+	return (hash_djb2(key) % size);
 }
