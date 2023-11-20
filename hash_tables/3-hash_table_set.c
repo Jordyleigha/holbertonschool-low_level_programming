d91ebad386e71a022952cb6b8e431405c527f387
Jordan <jordan.arguelles.ja@gmail.com>
2023-11-20 14:47:09 -0800

    writing my function

---
+++ hash_tables/3-hash_table_set.c
@@ -0,0 +1,47 @@
+#include "hash_tables.h"
+/**
+ * hash_table_set - adds an element in a hash table
+ * @ht: a pointer to the hash table
+ * @key: the key to add
+ * @value: the value of the key
+ *
+ * Return: 1 if it succeeded, 0 otherwise
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *add;
+	char *val;
+	unsigned long int index, n;
+
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	for (n = index; ht->array[n]; n++)
+	{
+		if (strcmp(ht->array[n]->key, key)==0)
+		{
+			free(ht->array[n]->value);
+			ht->array[n]->value = val;
+			return (1);
+		}
+	}
+
+	add = malloc(sizeof(hash_node_t));
+	if (add == NULL)
+	{
+		free(val);
+		return (0);
+	}
+	add->key = strdup(key);
+	if (add->key == NULL)
+	{
+		free(add);
+		return (0);
+	}
+	add->value = val;
+	add->next = ht->array[index];
+	ht->array[index] = add;
+
+	return (1);
+}
