90097225ee38b9ac7752b47cf0762acf0b347957
Jordan <jordan.arguelles.ja@gmail.com>
2023-11-20 11:22:11 -0800

    writing my function

--- doubly_linked_lists/6-sum_dlistint.c
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -10,7 +10,7 @@ int sum_dlistint(dlistint_t *head)
 	int sum = 0;
 	dlistint_t *current = head;
 
-	while (currrent != NULL)
+	while (current != NULL)
 	{
 		sum += current->n;
 		current = current->next;
