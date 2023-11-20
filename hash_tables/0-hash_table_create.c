ba3a3984cc7117f558a0a00a9b980b3faabfb88d
Jordan <jordan.arguelles.ja@gmail.com>
2023-11-20 14:03:21 -0800

    writing my function

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,17 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - creates a hash table
+ * @size: the size of the array
+ *
+ * Return: a pointer to the newly created hash table
+ * or if something went wrong return NULL
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	unsigned long int size = 0
+
+		for (int n = 0; str[n]; n++)
+			size += str[n];
+
+	return (size)
+}
