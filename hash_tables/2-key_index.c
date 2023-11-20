7a6a33f3ff1fd1fe0400533092be45229b307839
Jordan <jordan.arguelles.ja@gmail.com>
2023-11-20 14:25:49 -0800

    writing my declarations

---
+++ hash_tables/2-key_index.c
@@ -0,0 +1,11 @@
+#include "hash_tables.h"
+/**
+ * key_index - get the index at which a key/value should be 
+ * stored in array of a hasht table
+ * @key: the key to get the index of
+ * @size: the size of the array
+ *
+ * Return: the index of the key
+ * Decription: uses the djb2 algorithm
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
