8711fc2e918fe39453b84975200969dcbe53eb34
Jordan <jordan.arguelles.ja@gmail.com>
2023-11-20 10:47:33 -0800

    fixing whitespace

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -9,15 +9,15 @@
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *add_node;
-	
+
 	add_node = malloc(sizeof(dlistint_t));
-	if(add_node == NULL)
+	if (add_node == NULL)
 		return (NULL);
 
 	add_node->n = n;
 	add_node->prev = NULL;
 	add_node->next = *head;
-	if(*head != NULL)
+	if (*head != NULL)
 		(*head)->prev = add_node;
 	*head = add_node;
 
